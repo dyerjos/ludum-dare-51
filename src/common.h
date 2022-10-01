32e6fc70cb89a5a8bafb51597920a084330f726c
Joshua Dyer <dyerjo@mail.gvsu.edu>
2022-09-30 20:37:22 -0500

    fix assert bug

--- src/common.h
+++ src/common.h
@@ -11,7 +11,7 @@ do { \
     if (!(c)) { \
         fprintf(stderr, __FILE__ ":%d: assertion %s failed: %s\n", \
                         __LINE__, #c, m); \
-exit(1); \ }\
+exit(1);  }\
 } while(0)
 #else
 #define ASSERT(c, m)  // Empty macro if not enabled
